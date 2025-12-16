9b775997d89d0078e8f312c46425476b3306b87a
Laghrabi <laghrabichaimae>
2025-12-16 16:34:45 +0100

    completed ex04 and working on ex05

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: claghrab <claghrab@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 17:46:19 by claghrab          #+#    #+#             */
-/*   Updated: 2025/12/13 18:34:44 by claghrab         ###   ########.fr       */
+/*   Updated: 2025/12/16 13:48:01 by claghrab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,30 +17,39 @@
 
 int main(int ac, char **av)
 {
-    size_t i;
-    std::ifstream infile(av[1]);
-    std::string s1 = av[2], s2 = av[3];
-    std::stringstream buffer;
-    std::string content;
-    
-    if (ac < 4)
+    if (ac != 4 || av[2][0] == '\0')
     {
         std::cerr << "Wrong Arguments" << std::endl;
         return (1);
     }
-    if (infile.is_open() == false)
+    
+    size_t prev_pos, pos;
+    std::string filename = av[1];
+    std::ifstream infile(filename.c_str());
+    std::ofstream outfile((filename + ".replace").c_str());
+    std::string s1 = av[2], s2 = av[3];
+    std::stringstream buffer;
+    std::string content;
+    
+    if (infile.is_open() == false || outfile.is_open() == false)
     {
         std::cerr << "Error: Could not open input file." << std::endl;
         return (1);
     }
     else
     {
-        i = 0;
         buffer << infile.rdbuf();
         content = buffer.str();
-        while (content.find(s1, i) != std::string::npos)
+        pos = prev_pos = 0;
+        pos = content.find(s1, pos);
+        while (pos != std::string::npos)
         {
-            
+            outfile << content.substr(prev_pos, pos - prev_pos) << s2;
+            pos += s1.length();
+            prev_pos = pos;
+            pos = content.find(s1, pos);
         }
+        outfile << content.substr(prev_pos, pos - prev_pos);
     }
+    return (0);
 }
