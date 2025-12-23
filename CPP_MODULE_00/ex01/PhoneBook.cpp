1e087b574a2a37340c73fe35ad2e4a4a9a15006e
Laghrabi <laghrabichaimae@gmail.com>
2025-12-23 11:39:00 +0100

    refactor: optimize search() with helper functions and fix input handling

--- CPP_MODULE_00/ex01/PhoneBook.cpp
+++ CPP_MODULE_00/ex01/PhoneBook.cpp
@@ -2,7 +2,7 @@
 
 PhoneBook::PhoneBook()
 {
-    index = 0;
+    _index = 0;
 }
 void    PhoneBook::add()
 {
@@ -14,7 +14,7 @@ void    PhoneBook::add()
         std::cout << "Enter First Name: ";
         if (!getline(std::cin, input))
 			exit(0);
-        ArrOfCon[index].setFirstName(input);
+        ArrOfCon[_index].setFirstName(input);
     }
     input = "";
     while (input == "")
@@ -22,7 +22,7 @@ void    PhoneBook::add()
         std::cout << "Enter Last Name: ";
         if (!getline(std::cin, input))
 			exit(0);
-        ArrOfCon[index].setLastName(input);
+        ArrOfCon[_index].setLastName(input);
     }
     input = "";
     while (input == "")
@@ -30,7 +30,7 @@ void    PhoneBook::add()
         std::cout << "Enter Nickname: ";
         if (!getline(std::cin, input))
 			exit(0);
-        ArrOfCon[index].setNickName(input);
+        ArrOfCon[_index].setNickName(input);
     }
     input = "";
     while (input == "")
@@ -38,7 +38,7 @@ void    PhoneBook::add()
         std::cout << "Enter Phone Number: ";
         if (!getline(std::cin, input))
 			exit(0);
-        ArrOfCon[index].setPhoneNumber(input);
+        ArrOfCon[_index].setPhoneNumber(input);
     }
     input = "";
     while (input == "")
@@ -46,64 +46,82 @@ void    PhoneBook::add()
         std::cout << "Enter Darkest Secret: ";
         if (!getline(std::cin, input))
 			exit(0);
-        ArrOfCon[index].setDarkestSecret(input);
+        ArrOfCon[_index].setDarkestSecret(input);
     }
-    index++;
-    index %= 8;
+    _index++;
+    _index %= 8;
 }
 
-void	PhoneBook::search()
+std::string PhoneBook::_formatColumn(std::string str)
 {
-	int			i;
-	std::string	index;
+    if (str.length() > 10)
+        return str.substr(0, 9) + ".";
+    return (str);
+}
 
-	i = 0;
-	while (i < 8 && ArrOfCon[i].getFirstName() != "")
-	{
-		std::cout << "|" << std::setw(10);
-		std::cout << i << "|" << std::setw(10);
-		if (ArrOfCon[i].getFirstName().length() < 10)
-			std::cout << ArrOfCon[i].getFirstName();
-		else
-			std::cout << ArrOfCon[i].getFirstName().substr(0, 9) << ".";
-		std::cout << "|" << std::setw(10);
-		if (ArrOfCon[i].getLastName().length() < 10)
-			std::cout << ArrOfCon[i].getLastName();
-		else
-			std::cout << ArrOfCon[i].getLastName().substr(0, 9) << ".";
-		std::cout << "|" << std::setw(10);
-		if (ArrOfCon[i].getNickName().length() < 10)
-			std::cout << ArrOfCon[i].getNickName();
-		else
-			std::cout << ArrOfCon[i].getNickName().substr(0, 9) << ".";
-		std::cout << "|";
-		std::cout << std::endl;
-		i++;
-	}
-	index = "";
-	while (index[0] < 48 || index[0] > 57)
-	{
-		std::cout << "Enter Index: ";
-		if (!getline(std::cin, index))
-			exit(0);
-		std::cout << "Index must be a digit." << std::endl;
-	}
-	i = std::atoi(index.c_str());
-	while (i < 0 || i > 7)
-	{
-		std::cout << "Index Must Be Between 0 And 7.\nEnter Index: ";
-		if (!getline(std::cin, index))
-			exit(0);
-		i = std::atoi(index.c_str());
-	}
-	if (ArrOfCon[i].getFirstName() == "")
-		std::cout << "Contact Doesn't Exist\n";
-	else
-	{
-		std::cout << "FirstName: " << ArrOfCon[i].getFirstName() << std::endl;
-		std::cout << "LastName: " << ArrOfCon[i].getLastName() << std::endl;
-		std::cout << "NickName: " << ArrOfCon[i].getNickName() << std::endl;
-		std::cout << "PhoneNumber: " << ArrOfCon[i].getPhoneNumber() << std::endl;
-		std::cout << "DarkestSecret: " << ArrOfCon[i].getDarkestSecret() << std::endl;
-	}
+void    PhoneBook::_displayTable()
+{
+    std::cout << " ___________________________________________ " << std::endl;
+    std::cout << "|     Index|First Name| Last Name|  Nickname|" << std::endl;
+    std::cout << "|----------|----------|----------|----------|" << std::endl;
+
+    for (int i = 0; i < 8; i++)
+    {
+        if (ArrOfCon[i].getFirstName().empty())
+            break ;
+        std::cout << "|" << std::setw(10) << i;
+        std::cout << "|" << std::setw(10) << _formatColumn(ArrOfCon[i].getFirstName());
+        std::cout << "|" << std::setw(10) << _formatColumn(ArrOfCon[i].getLastName());
+        std::cout << "|" << std::setw(10) << _formatColumn(ArrOfCon[i].getNickName());
+        std::cout << "|" << std::endl;
+    }
+    std::cout << " ------------------------------------------- " << std::endl;
+}
+
+int     PhoneBook::_readInputIndex()
+{
+    std::string input;
+    int         index = -1;
+    bool        valid = false;
+
+    while (!valid)
+    {
+        std::cout << "Enter Index: ";
+        if (!std::getline(std::cin, input))
+            exit(0);
+
+        if (input.empty()) 
+            continue;
+        
+        bool isDigit = true;
+        for (size_t j = 0; j < input.length(); j++) {
+            if (!isdigit(input[j])) isDigit = false;
+        }
+
+        if (isDigit)
+        {
+            index = std::atoi(input.c_str());
+            if (index >= 0 && index < 8 && !ArrOfCon[index].getFirstName().empty())
+                valid = true;
+            else
+                std::cout << "Invalid index." << std::endl;
+        }
+        else
+            std::cout << "Invalid input (digits only)." << std::endl;
+    }
+    return (index);
+}
+
+void    PhoneBook::search()
+{
+    _displayTable();
+
+    int i = _readInputIndex();
+
+    std::cout << "\n--- Contact Details ---" << std::endl;
+    std::cout << "First Name:    " << ArrOfCon[i].getFirstName() << std::endl;
+    std::cout << "Last Name:     " << ArrOfCon[i].getLastName() << std::endl;
+    std::cout << "Nickname:      " << ArrOfCon[i].getNickName() << std::endl;
+    std::cout << "Phone Number:  " << ArrOfCon[i].getPhoneNumber() << std::endl;
+    std::cout << "Darkest Secret:" << ArrOfCon[i].getDarkestSecret() << std::endl;
 }
--- CPP_MODULE_00/ex01/PhoneBook.hpp
+++ CPP_MODULE_00/ex01/PhoneBook.hpp
@@ -3,14 +3,20 @@
 
 #include "contact.hpp"
 
-class PhoneBook {
-	private:
-		Contact	ArrOfCon[8];
-		int		index;
-	public:
-		void	add();
-		void	search();
-		PhoneBook();
+class PhoneBook
+{
+    private:
+        Contact     ArrOfCon[8];
+        int         _index;
+
+        std::string _formatColumn(std::string str); 
+        void        _displayTable();
+        int         _readInputIndex();
+
+    public:
+        PhoneBook();
+        void    add();
+        void    search();
 };
 
 #endif
