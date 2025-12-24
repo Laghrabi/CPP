1b7bd28cf980f0ef0c40c2b5f3868232f88b9f95
Laghrabi <laghrabichaimae>
2025-12-24 15:53:32 +0100

    completed ex00, working on ex01

---
+++ CPP_MODULE_02/ex00/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: claghrab <claghrab@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/24 15:49:17 by claghrab          #+#    #+#             */
+/*   Updated: 2025/12/24 15:49:40 by claghrab         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main( void ) {
+Fixed a;
+Fixed b( a );
+Fixed c;
+c = b;
+std::cout << a.getRawBits() << std::endl;
+std::cout << b.getRawBits() << std::endl;
+std::cout << c.getRawBits() << std::endl;
+return 0;
+}
